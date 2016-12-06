28efd186e64912a4ed4c0f708f1dec6bf4b30fc2
Jan Kurs <kurs.jan@gmail.com>
2016-12-06 07:57:37 +0100

    dependency isues, bug fixes, reverted pharo.image to clean version, use PharoPP2 for building the book

--- pillar-book/loadPetitParser2.sh
+++ pillar-book/loadPetitParser2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./pharo "Pharo.image" eval --save "
+./pharo "Pharo.image" save "PharoPP2"
+
+./pharo "PharoPP2.image" eval --save "
+| repo |
 repo := './..' asFileReference.
 repo := repo absolutePath segments inject: 'filetree://' into: [ :path :seg | path, '/', seg ].
 Metacello new
